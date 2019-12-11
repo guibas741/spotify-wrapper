cfb544fea42625f5fa8075b850bd583abd182e52
Guilherme Augusto <guilherme.bublitz@disruptiva.com.br>
2019-12-10 22:53:55 -0200

    Implementa testes dos metodos de album

--- src/album.js
+++ src/album.js
@@ -1,2 +1,5 @@
-export const getAlbum = () => {};
+export const getAlbum = (id) => {
+  return fetch(`https://api.spotify.com/v1/albums/${id}`)
+    .then((data) => data.json());
+};
 export const getAlbumTracks = () => {};
--- tests/album.spec.js
+++ tests/album.spec.js
@@ -1,9 +1,28 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-undef */
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
 import { getAlbum, getAlbumTracks } from '../src/album';
 
+chai.use(sinonChai);
+
+global.fetch = require('node-fetch');
+
 describe('Album', () => {
+  let fetchedStub;
+  let promise;
+
+  beforeEach(() => {
+    fetchedStub = sinon.stub(global, 'fetch');
+    promise = fetchedStub.resolves({ json: () => ({ album: 'name' }) });
+  });
+
+  afterEach(() => {
+    fetchedStub.restore();
+  });
+
   describe('smoke tests', () => {
     it('should have getAlbum method', () => {
       expect(getAlbum).to.exist;
@@ -11,6 +30,26 @@ describe('Album', () => {
 
     it('should have getAlbumTracks method', () => {
       expect(getAlbumTracks).to.exist;
+    });
+  });
+
+  describe('getAlbum', () => {
+    it('should call fetch method', () => {
+      const album = getAlbum();
+      expect(fetchedStub).to.be.calledOnce;
+    });
+
+    it('should call the correct URL', () => {
+      const album = getAlbum('4aawyAB9vmqN3uQ7FjRGTy');
+      expect(fetchedStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy');
+    });
+
+    it('should return the correct data from the promise', () => {
+      const album = getAlbum('4aawyAB9vmqN3uQ7FjRGTy');
+      album.then((data) => {
+        expect(data).to.be.eql({});
+      });
     });
   });
 });
