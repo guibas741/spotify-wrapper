2d622dafa083b1bf5ef052aa9895862da9302495
Guilherme Augusto <guilherme.bublitz@disruptiva.com.br>
2019-12-10 22:32:13 -0200

    First commit

---
+++ examples/albums.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { searchAlbums } from '../src/main';
+
+global.fetch = require('node-fetch');
+
+const albums = searchAlbums('Incubus');
+
+albums.then((data) => console.log(data));
