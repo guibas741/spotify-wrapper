d705743a02af7195435def6c28951addfbd978be
Guilherme Augusto <guilherme.bublitz@disruptiva.com.br>
2019-12-12 18:47:00 -0200

    Adiciona testes de construtores

--- src/index.js
+++ src/index.js
@@ -11,14 +11,11 @@ import {
   getAlbums,
   getAlbumTracks,
 } from './album';
+import { API_URL } from './config';
 
-module.exports = {
-  search,
-  searchArtists,
-  searchAlbums,
-  searchTracks,
-  searchPlaylists,
-  getAlbum,
-  getAlbums,
-  getAlbumTracks,
-};
+export default class SpotifyWrapper {
+  constructor(options) {
+    this.apiURL = options.apiURL || API_URL;
+    this.token = options.token;
+  }
+}
